e7d794a0016abda8c973b33847b1dd8c54cece9f
Kyle Colantonio <kyle10468@gmail.com>
2014-03-09 16:21:39 -0400

    Updated StringUtils

--- src/net/skaianet/utils/StringUtils.java
+++ src/net/skaianet/utils/StringUtils.java
@@ -9,4 +9,8 @@ public final class StringUtils {
 		}
 		return oString.trim();
 	}
+	
+	public static final String replaceVariables(String str, String admin, String ip, String reason, String time, String victim) {
+		return ChatUtils.colorize(str.replaceAll("%admin%", admin).replaceAll("%ip%", ip).replaceAll("%reason%", reason).replaceAll("%time%", time).replaceAll("%victim%", victim));
+	}
 }
